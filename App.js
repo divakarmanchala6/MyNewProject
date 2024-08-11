09649d900cf4988b30195c66876c0ad9db8b5e59
divakarmanchala6 <divakar.manchala6@gmail.com>
2024-08-11 12:22:41 +0530

    added counter text

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.count}>Count {this.state.count}</Text>
+        <Text>+</Text>
+        <Text>-</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: '100%',
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   },
-});
+
+  count: {
+    fontSize: 45,
+  },
+
+  
+})
+
+export default App
